c58108f7790d2405f3f0936b4bf99b0eb91f3af4
polenger <polenger@qq.com>
2018-10-04 10:27:17 +0800

    Initial commit

---
+++ AjaxJsonp/src/main/java/com/loikun/ajaxjsop/AjaxjsopApplication.java
@@ -0,0 +1,12 @@
+package com.loikun.ajaxjsop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxjsopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxjsopApplication.class, args);
+    }
+}
